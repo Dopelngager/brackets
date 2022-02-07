69ca1979d1ddb972d3e2bd052a579f8bf23964d5
Vnukovskiy <vnukovskiy.dmitriy@gmail.com>
2022-02-07 13:15:36 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let newArr = [...str];
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (
+        newArr[i] === bracketsConfig[j][0] &&
+        newArr[i + 1] === bracketsConfig[j][1]
+      ) {
+        newArr.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+  if (newArr.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
